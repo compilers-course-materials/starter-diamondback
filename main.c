97e473728149abd7e99b4597bb8dd220392746f9
Joe Gibbs Politz <joe.politz@gmail.com>
2016-02-24 10:52:29 -0800

    .

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 extern int our_code_starts_here() asm("our_code_starts_here");
-extern void error() asm("error");
 extern int print(int val) asm("print");
 
 int print(int val) {
@@ -22,25 +21,14 @@ int print(int val) {
   return val;
 }
 
-void error(int i) {
-  if (i == 0) {
-    fprintf(stderr, "Error: comparison operator got boolean");
-  }
-  else if (i == 1) {
-    fprintf(stderr, "Error: arithmetic operator got boolean");
-  }
-  else if (i == 2) {
-    fprintf(stderr, "Error: if condition got number");
-  }
-  else if (i == 3) {
-    fprintf(stderr, "Error: Integer overflow");
-  }
-  else {
-    fprintf(stderr, "Error: Unknown error code: %d\n", i);
-  }
-  exit(i);
-}
+/*
 
+Copy over any error-detection functions here
+
+*/
+
+
+// main should remain unchanged
 int main(int argc, char** argv) {
   int result = our_code_starts_here();
   print(result);
